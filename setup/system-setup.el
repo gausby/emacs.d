0cefb91c50c93681cd62811d43bc9b6f667e7575
Martin Gausby <martin@gausby.dk>
2015-06-24 22:26:39 +0200

    revert files when they change on the file system

--- setup/system-setup.el
+++ setup/system-setup.el
@@ -9,6 +9,9 @@
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 
+;; revert files in buffers when the underlying files change
+(global-auto-revert-mode t)
+
 (provide 'system-setup)
 
 ;;; system-setup.el ends here
