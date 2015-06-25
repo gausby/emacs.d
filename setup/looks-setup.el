c16fb52f2e70b70487eb0557986d272250c285be
Martin Gausby <martin@gausby.dk>
2015-06-25 21:53:21 +0200

    I need to figure out how to do if-blocks in elisp

--- setup/looks-setup.el
+++ setup/looks-setup.el
@@ -11,9 +11,9 @@
 
 ;; font and font sizes
 (if (equal tty-erase-char 0)
-    (set-face-attribute 'default nil :font "--source code pro-normal-r-normal-normal-18--128-128-c-*-iso10646-1")
-  (set-face-attribute 'mode-line nil :font "--source code pro-normal-r-normal-normal-14--128-128-c-*-iso10646-1")
-  )
+    (set-face-attribute 'default nil :font "--source code pro-normal-r-normal-normal-18--128-128-c-*-iso10646-1"))
+(if (equal tty-erase-char 0)
+    (set-face-attribute 'mode-line nil :font "--source code pro-normal-r-normal-normal-12--128-128-c-*-iso10646-1"))
 
 ;; colors and theme
 (setq ns-use-srgb-colorspace t)
