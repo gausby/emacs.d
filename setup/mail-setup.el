353e5283ba1ea3ab73e6563c05b88282571fddbf
Martin Gausby <martin@gausby.dk>
2015-07-14 11:45:38 +0200

    updated the saved notmuch searches

--- setup/mail-setup.el
+++ setup/mail-setup.el
@@ -5,18 +5,20 @@
 ;; This will:
 ;;
 ;;   * Add a hook for always signing messages
+;;   * Configure saved searches for my notmuch setup
 
 ;;; Code:
 (require 'notmuch)
 
 (setq notmuch-saved-searches
-      '((:name "inbox" :query "tag:inbox" :key "i")
-        (:name "unread" :query "tag:unread" :key "u")
+      '((:name "unread" :query "tag:unread and not tag:mailing-list" :key "u")
+        (:name "inbox" :query "tag:inbox" :key "i")
         (:name "flagged" :query "tag:flagged" :key "f")
         (:name "sent" :query "tag:sent" :key "t")
+        (:name "today" :query "tag:inbox and date:-24h..now and not tag:mailing-list" :key "c")
         (:name "drafts" :query "tag:draft" :key "d")
         (:name "all mail" :query "*" :key "a")
-        (:name "today" :query "date:today..now")))
+        (:name "mailing-lists" :query "tag:mailing-list" :count-query "tag:mailing-list and tag:unread" :key "m")))
 
 ;; mode hooks
 (add-hook 'message-setup-hook 'mml-secure-message-sign-pgpmime)
