61db3f6fab52dc4ad1913cd2a9d63cfac8a10e23
Martin Gausby <martin@gausby.dk>
2015-07-18 13:09:51 +0200

    added commentary to company setup and fixed style

--- setup/company-setup.el
+++ setup/company-setup.el
@@ -1,16 +1,19 @@
 ;;; company-setup.el --- completion setup
 
 ;;; Commentary:
-;;
+;;  * Set idle delay a bit long to stop a conflict with auto completing do..end
+;;    blocks in files where a function starting with `do_` would complete before
+;;    the auto-pair
+;;  * Company mode is global
 
 ;;; Code:
 
 (require 'company)
 
-(setq company-idle-delay 0.75)
-(setq company-tooltip-limit 10)
-(setq company-minimum-prefix-length 2)
-(setq company-tooltip-flip-when-above t)
+(setq company-idle-delay 0.7
+      company-tooltip-limit 10
+      company-minimum-prefix-length 2
+      company-tooltip-flip-when-above t)
 
 (global-company-mode 1)
 
