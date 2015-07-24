ef9214b64ca139e3d20fe4e317c39fd1b34b0877
Martin Gausby <martin@gausby.dk>
2015-07-24 14:59:18 +0200

    fixed the alchemist test buffer name in popwin

--- setup/popwin-setup.el
+++ setup/popwin-setup.el
@@ -10,7 +10,7 @@
 (push '(elixir-mix-task-runner) popwin:special-display-config)
 (push '("*Messages*" :noselect t :stick t) popwin:special-display-config)
 (push '("*compilation*" :height 25 :noselect t :stick t) popwin:special-display-config)
-(push '("*mix*" :height 25 :noselect t) popwin:special-display-config)
+(push '("*alchemist-test-report*" :height 25 :stick t) popwin:special-display-config)
 (push '("*alchemist message*" :height 20 :noselect t) popwin:special-display-config)
 (push '("*ag search*" :height 20 :dedicated t) popwin:special-display-config)
 
