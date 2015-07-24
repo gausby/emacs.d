00e90855beb315644112ebea4fed30c3e5c680eb
Martin Gausby <martin@gausby.dk>
2015-07-24 14:11:57 +0200

    fixing colorizing for erc mode

--- setup/erc-setup.el
+++ setup/erc-setup.el
@@ -3,22 +3,34 @@
 ;;; Commentary:
 ;;  Setup for erc and znc
 ;;
+;;  * Enable spelling mode for erc buffers
+;;  * Disable fill mode, makes it easier to search using erc-occur
 ;;  * Keybindings should be prefixed with `C-c i`, i for "irc"
 ;;  * Switch to Next unread erc buffer on `C-c i n` and back to the previous
 ;;    non erc buffer when there is no more unread buffers (n for next)
-;;  * Disable/Enable track mode with `C-C i t` (t for track)
+;;  * Disable/Enable track mode with `C-c i t` (t for track)
+;;  * Disable/Enable timestamps with `C-c i d` (d for 'date-time')
+;;  * Search in erc buffers with `C-c i s` (s for search)
 
 ;;; Code:
 (require 'erc)
+(require 'erc-stamp)
 
 (erc-colorize-mode 1)
+(erc-spelling-mode 1)
 
 ;; in chat -----------------------------------------------------------------
-;; time stamps
+(defun my-erc-mode-hook ()
+  (erc-fill-disable)
+  (visual-line-mode))
+(add-hook 'erc-mode-hook 'my-erc-mode-hook)
+
 (setq erc-timestamp-only-if-changed-flag t
-      erc-timestamp-format "%H:%M "
-      erc-fill-prefix "        "
-      erc-insert-timestamp-function 'erc-insert-timestamp-left)
+      erc-insert-timestamp-function 'erc-insert-timestamp-right
+      erc-fill-column nil
+      erc-timestamp-format "[%H:%M]"
+      erc-hide-timestamps t)
+
 ;; messages
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 
@@ -34,6 +46,8 @@
 ;; Switch to unread buffer
 (define-key global-map (kbd "C-c i n") 'erc-track-switch-buffer)
 (define-key global-map (kbd "C-c i t") 'erc-track-mode)
+(define-key global-map (kbd "C-c i d") 'erc-toggle-timestamps)
+(define-key global-map (kbd "C-c i s") 'erc-occur)
 
 (provide 'erc-setup)
 
