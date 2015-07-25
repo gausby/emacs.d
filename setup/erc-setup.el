16213327826c032656fb4c889659c96a0c3f56ee
Martin Gausby <martin@gausby.dk>
2015-07-25 15:55:27 +0200

    added keybindings for connecting to znc C-c i c a

--- setup/erc-setup.el
+++ setup/erc-setup.el
@@ -6,6 +6,7 @@
 ;;  * Enable spelling mode for erc buffers
 ;;  * Disable fill mode, makes it easier to search using erc-occur
 ;;  * Keybindings should be prefixed with `C-c i`, i for "irc"
+;;  * Connect to all servers using `C-c i c a`, connect to only one using `C-c i c c`
 ;;  * Switch to Next unread erc buffer on `C-c i n` and back to the previous
 ;;    non erc buffer when there is no more unread buffers (n for next)
 ;;  * Disable/Enable track mode with `C-c i t` (t for track)
@@ -43,7 +44,9 @@
 ;; Get rid of the iswitchb obsolete message when hitting C-c C-b
 (eval-after-load "erc" '(define-key erc-mode-map (kbd "C-c C-b") nil))
 
-;; Switch to unread buffer
+;; define keybindings
+(define-key global-map (kbd "C-c i c a") 'znc-all)
+(define-key global-map (kbd "C-c i c c") 'znc-erc)
 (define-key global-map (kbd "C-c i n") 'erc-track-switch-buffer)
 (define-key global-map (kbd "C-c i t") 'erc-track-mode)
 (define-key global-map (kbd "C-c i d") 'erc-toggle-timestamps)
