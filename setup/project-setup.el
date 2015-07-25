f76035a225182d4d2441c4d6027f2f1372a9b1e1
Martin Gausby <martin@gausby.dk>
2015-07-26 00:09:29 +0200

    made the font in neotree a bit smaller

--- setup/project-setup.el
+++ setup/project-setup.el
@@ -8,6 +8,7 @@
 ;;  * Highlight search term in silver searcher results
 ;;  * Set up neotree
 ;;  * Focus neotree on current project root with `C-c p .`
+;;  * Make the neotree font a bit smaller
 
 ;;; Code:
 (require 'neotree)
@@ -23,11 +24,16 @@
 
 ;; Neotree
 (global-set-key (kbd "C-c .") 'neotree-toggle)
-(define-key global-map (kbd "C-c p .") 'neotree-projectile-action)
+(global-set-key (kbd "C-c p .") 'neotree-projectile-action)
 
 (setq neo-banner-message nil
       neo-theme 'arrow
       neo-window-width 20)
+
+;; neotree-mode-hook
+(defun my-neotree-mode-hook ()
+  (text-scale-set -3))
+(add-hook 'neotree-mode-hook 'my-neotree-mode-hook)
 
 (provide 'project-setup)
 
