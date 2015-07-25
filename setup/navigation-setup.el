9e4496a43bf94661746d20ea8a730c2019754982
Martin Gausby <martin@gausby.dk>
2015-07-26 00:25:15 +0200

    changed the avy keybindings to use a `M-g` prefix
      * `M-g SPC` = avy-goto-char
      * `M-g l` = avy-goto-line
      * `M-g w` = avy-goto-word-or-subword-1
    
    This is done because goto-line is bound to `M-g g`, and because `M-g SPC` is
    fairly quickly typed.

--- setup/navigation-setup.el
+++ setup/navigation-setup.el
@@ -2,13 +2,16 @@
 
 ;;; Commentary:
 ;;  Setup for the various navigation modes
-;;    * Use avy to navigate to chars or lines in buffer
+;;
+;;  * Use avy to navigate to chars or lines in buffer
+;;  * Keyboard prefix for avy is `M-g` because goto-line resides on `M-g g`
 
 ;;; Code:
 (require 'avy)
 
-(global-set-key (kbd "C-c SPC") 'avy-goto-word-or-subword-1)
-(global-set-key (kbd "C-c C-SPC") 'avy-goto-line)
+(global-set-key (kbd "M-g SPC") 'avy-goto-char)
+(global-set-key (kbd "M-g l") 'avy-goto-line)
+(global-set-key (kbd "M-g w") 'avy-goto-word-or-subword-1)
 
 (provide 'navigation-setup)
 
