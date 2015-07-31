c48c3fee97114b8ae14762313732ba3b65b8f71d
Martin Gausby <martin@gausby.dk>
2015-07-31 21:25:25 +0200

    added a keybinding for toggeling Sauron `C-c n n`

--- setup/notifications-setup.el
+++ setup/notifications-setup.el
@@ -10,7 +10,16 @@
       sauron-hide-mode-line t)
 
 (setq sauron-watch-nicks '("gausby")
-      sauron-nick-insensitivity 120)
+      sauron-nick-insensitivity 60)
+
+;; sauron-mode-hook
+(defun mg/sauron-mode-hook ()
+  (text-scale-set -3))
+(add-hook 'sauron-mode-hook 'mg/sauron-mode-hook)
+
+;; Key bindings
+(global-set-key (kbd "C-c n n") 'sauron-toggle-hide-show)
+(global-set-key (kbd "C-c n c") 'sauron-clear)
 
 (provide 'notifications-setup)
 
