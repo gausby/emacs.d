781878046ce9f7452f57a5ab8fad14385358ffa5
Martin Gausby <martin@gausby.dk>
2015-08-29 00:20:53 +0200

    Restructure sections of the erc setup file

--- setup/erc-setup.el
+++ setup/erc-setup.el
@@ -40,6 +40,12 @@
 ;; messages
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 
+;; tracking ----------------------------------------------------------------
+(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+                                "324" "329" "332" "333" "353" "477")
+      ;; ...and please stop complaining about my key bindings!
+      erc-track-enable-keybindings nil)
+
 ;; log to disk
 (erc-log-mode)
 (setq erc-log-channels-directory "~/.erc/logs/"
@@ -50,6 +56,7 @@
       erc-log-write-after-send t
       erc-log-file-coding-system 'utf-8)
 
+;; searching logs
 (defun mg/search-erc-logs (term)
   "Search the irc logs for a given term"
   (interactive "sTerm to search for in the logs: ")
@@ -60,12 +67,6 @@
 
 (add-to-list 'auto-mode-alist
              `(,(format "%s.*\\.txt" (regexp-quote (expand-file-name erc-log-channels-directory))) . erc-view-log-mode))
-
-;; tracking ----------------------------------------------------------------
-(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
-                                "324" "329" "332" "333" "353" "477")
-      ;; ...and please stop complaining about my key bindings!
-      erc-track-enable-keybindings nil)
 
 ;; Get rid of the iswitchb obsolete message when hitting C-c C-b
 (eval-after-load "erc" '(define-key erc-mode-map (kbd "C-c C-b") nil))
