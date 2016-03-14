daaa78e9a6ee8a22e8d8530af2d2f7e8ada658bf
Martin Gausby <martin@gausby.dk>
2016-03-13 22:16:04 -0700

    Easier to expand-region on American keyboard

--- setup/expand-region-setup.el
+++ setup/expand-region-setup.el
@@ -6,7 +6,7 @@
 ;;; Code:
 (require 'expand-region)
 
-(global-set-key (kbd "C-+") 'er/expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
 (global-set-key (kbd "C--") 'er/contract-region)
 
 (provide 'expand-region-setup)
