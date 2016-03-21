8d75c2ca8ffa64c303f48e69ef0ec9b7a39f36f9
Martin Gausby <martin@gausby.dk>
2016-03-21 15:21:25 -0700

    Enable visual line mode for markdown buffers

--- setup/markdown-mode-setup.el
+++ setup/markdown-mode-setup.el
@@ -1,7 +1,8 @@
 ;;; markdown-mode-setup.el --- setup for markdown-mode
 
 ;;; Commentary:
-;;  Setup for markdown mode
+;;  - Setup for markdown mode, use github flavored markdown
+;;  - Enable visual line mode for markdown buffers
 
 ;;; Code:
 (require 'markdown-mode)
@@ -10,6 +11,10 @@
 
 (add-to-list 'auto-mode-alist '("\\.markdown$" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
+
+(defun mg/gfm-mode-hook ()
+  (visual-line-mode +1))
+(add-hook 'gfm-mode-hook 'mg/gfm-mode-hook)
 
 (provide 'markdown-mode-setup)
 ;;; markdown-mode-setup.el ends here
