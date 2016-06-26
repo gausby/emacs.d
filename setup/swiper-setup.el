38a6abf8469bbfe3abeb980b40a741ae0543b879
Martin Gausby <martin@gausby.dk>
2016-06-26 21:19:39 +0200

    Use ivy when selecting stuff in projectile

--- setup/swiper-setup.el
+++ setup/swiper-setup.el
@@ -1,15 +1,19 @@
 ;;; swiper-setup.el --- setup for swiper
 
 ;;; Commentary:
-;;
+;;  * use ivy for completion and complleation
+;;  * activate ivy mode for projectile
 
 ;;; Code:
 (require 'swiper)
+(require 'projectile)
 
 (ivy-mode 1)
 
 ;; don't show recent closed items in various buffers
 (setq ivy-use-virtual-buffers nil)
+
+(setq projectile-completion-system 'ivy)
 
 ;; keybindings
 (global-set-key "\C-s" 'swiper)
