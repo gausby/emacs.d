2884a69f0cd490caf4fa973de2aa1feccfcfe0cf
Martin Gausby <martin@gausby.dk>
2017-01-05 21:50:25 +0100

    setup el-get and use the material theme

--- init.el
+++ init.el
@@ -1,3 +1,21 @@
+;; paths
+(setq emacs-config-dir (file-name-directory
+                        (or (buffer-file-name) load-file-name)))
+
+(add-to-list 'load-path (concat emacs-config-dir "/site/"))
+
+
+;;; package.el configuration
+(require 'package)
+(dolist (arch '(("gnu" . "http://elpa.gnu.org/packages/")
+                ("melpa" . "https://melpa.org/packages/")
+                ("tromey" . "http://tromey.com/elpa/")
+                ))
+  (add-to-list 'package-archives arch))
+(package-initialize)
+
+
+;; el-get configuration
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 (unless (require 'el-get nil 'noerror)
@@ -8,4 +26,22 @@
     (eval-print-last-sexp)))
 
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
+
+(setq el-get-user-package-directory
+      (concat user-emacs-directory "/configs"))
+
+;; el-get-sources overrides
+(setq el-get-sources
+ '((:name material-theme
+	  :type elpa)
+   ))
+
+(setq my-packages
+      (append
+       '(el-get)
+       (mapcar 'el-get-source-name el-get-sources)))
+
+(el-get 'sync my-packages)
+
+(setq custom-safe-themes t)
+(load-theme 'material)
