cf9a612bc8c56bb8065bf44af3fdd11137074311
Martin Gausby <martin@gausby.dk>
2017-01-22 21:58:49 +0100

    Eval and attach the org mode hook when org is required

--- configs/org.el
+++ configs/org.el
@@ -1,17 +1,15 @@
-(el-get-bundle org-mode
-  (progn
-    (defun mg/org-mode-hook ()
-      "Hooks for org-mode"
-      (org-bullets-mode 1)
-      (visual-line-mode 1)
-      (set-visual-wrap-column 90)
-      (set-fill-column 90))
-    (add-hook 'org-mode-hook 'mg/org-mode-hook)))
-
+(el-get-bundle org-mode)
 (el-get-bundle org-bullets)
 
 (with-eval-after-load 'org
   (setq org-src-fontify-natively t     ;; highlight code blocks
         org-confirm-babel-evaluate nil ;; don't ask about code eval
         ;; hide asterisks and slashes for bold and italics
-        org-hide-emphasis-markers t))
+        org-hide-emphasis-markers t)
+  (defun mg/org-mode-hook ()
+      "Hooks for org-mode"
+      (org-bullets-mode 1)
+      (visual-line-mode 1)
+      (set-visual-wrap-column 90)
+      (set-fill-column 90))
+  (add-hook 'org-mode-hook 'mg/org-mode-hook))
