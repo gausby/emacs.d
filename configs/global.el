fd1e1e81662469d846c20e1f4cc1f14a6b9a610e
Martin Gausby <martin@gausby.dk>
2017-04-27 13:27:08 +0200

    Changed the color scheme of rainbow delimiters to my liking
    I find this order better for modes like Elixir and Erlang; I didn't
    like that the first level was red. I will try to use rainbow
    delimiters in more modes from now on.

--- configs/global.el
+++ configs/global.el
@@ -8,7 +8,8 @@
   (load-theme 'material))
 (with-eval-after-load 'material-theme
   ;; Remove the background box on headlines in org-mode buffers
-  (let* ((headline `(:background nil :box nil)))
+  (let ((headline `(:background nil :box nil))
+        (class '((class color) (min-colors 89))))
     (custom-theme-set-faces
      'user
      `(org-level-4 ((t (:height 1.0))))
@@ -19,7 +20,19 @@
      `(org-block-begin-line ((t (:box nil))))
      `(org-block-end-line ((t (:box nil))))
      `(org-block ((t (:foreground nil :background "#1e2930"))))
-     `(org-code ((t (:background "#1e2930")))))))
+     `(org-code ((t (:background "#1e2930"))))
+     ;; I find this rainbow-delimiters scheme better for non-lisp modes:
+     `(rainbow-delimiters-depth-1-face ((,class (:foreground ,"#C0C0C0"))))
+     `(rainbow-delimiters-depth-2-face ((,class (:foreground ,"#B388FF"))))
+     `(rainbow-delimiters-depth-3-face ((,class (:foreground ,"#26A69A"))))
+     `(rainbow-delimiters-depth-4-face ((,class (:foreground ,"#EF6C00"))))
+     `(rainbow-delimiters-depth-5-face ((,class (:foreground ,"#2196F3"))))
+     `(rainbow-delimiters-depth-6-face ((,class (:foreground ,"#FFCDD2"))))
+     `(rainbow-delimiters-depth-7-face ((,class (:foreground ,"#8BC34A"))))
+     `(rainbow-delimiters-depth-8-face ((,class (:foreground ,"#FFF59D"))))
+     `(rainbow-delimiters-depth-9-face ((,class (:foreground ,"#795548"))))
+     `(rainbow-delimiters-unmatched-face ((,class (:foreground ,"#F0F0F0" :background ,"#F36C60"))))
+    )))
 
 
 ;; disable files from being created
