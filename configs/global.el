df99c37be5f13c2f62446fd326a261a6792be3e1
Martin Gausby <martin@gausby.dk>
2017-06-27 23:12:09 +0200

    Made dired reuse the buffer when navigating using ^ and RET

--- configs/global.el
+++ configs/global.el
@@ -186,6 +186,15 @@
 
 
 ;;
+;; Dired
+;;
+;; Make dired reuse the buffer when navigating
+(put 'dired-find-alternate-file 'disabled nil)
+(define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+(define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file "..")))
+
+
+;;
 ;; Compilation mode
 ;;
 (with-eval-after-load 'compile
