be71ad3e81e3f182deeb55f41b020c519d996992
Martin Gausby <martin@gausby.dk>
2018-02-03 13:31:19 +0100

    Add ledger-mode
    This setup is configured to work with hledger.

--- configs/text-modes.el
+++ configs/text-modes.el
@@ -38,6 +38,28 @@
 
 
 ;;
+;; ledger (hledger) - plain text accounting
+;;
+(el-get-bundle ledger-mode)
+(with-eval-after-load 'ledger-mode
+  (setq ledger-binary-path "hledger"
+        ledger-mode-should-check-version nil
+        ledger-highlight-xact-under-point nil)
+  ;; Reports
+  (defvar ledger-report-balance
+    (list "bal" (concat ledger-binary-path " -f %(ledger-file) bal")))
+  (defvar ledger-report-reg
+    (list "reg" (concat ledger-binary-path " -f %(ledger-file) reg")))
+  (defvar ledger-report-account
+    (list "account" (concat ledger-binary-path " -f %(ledger-file) reg %(account)")))
+  (setq ledger-reports
+        (list ledger-report-balance
+              ledger-report-reg
+              ledger-report-account))
+  ;; register file types
+  (add-to-list 'auto-mode-alist '("\\.\\(h?ledger\\|journal\\|j\\)$" . ledger-mode)))
+
+;;
 ;; org-mode
 ;;
 (el-get-bundle org-mode)
